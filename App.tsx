78736618cfee4b21588208bbd0bf596641dabad4
Sairaj Kalkundre <sairajkalkundre@gmail.com>
2021-09-03 20:38:07 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+        <Text>Hello Sairaj</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
